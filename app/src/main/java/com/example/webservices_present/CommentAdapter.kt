f3b911e27564abee4096d34c7f2511aad47fb4eb
QuanLac <89645977+LacTQuan@users.noreply.github.com>
2024-03-22 00:30:14 +0700

    done

--- app/src/main/java/com/example/webservices_present/CommentAdapter.kt
+++ app/src/main/java/com/example/webservices_present/CommentAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.webservices_present
 
-class CommentAdapter {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CommentAdapter(private val context: Context, private val comments: ArrayList<Comment>) : RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView = itemView.findViewById(R.id.name)
+        val id: TextView = itemView.findViewById(R.id.commentId)
+        val email: TextView = itemView.findViewById(R.id.email)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.comment_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return comments.size
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, postition: Int) {
+        val comment = comments[postition]
+        holder.name.text = comment.name
+        holder.id.text = comment.id.toString()
+        holder.email.text = comment.email
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateData(newComments: ArrayList<Comment>) {
+        comments.clear()
+        comments.addAll(newComments)
+        notifyDataSetChanged()
+    }
+
 }
--- app/src/main/java/com/example/webservices_present/MainActivity.kt
+++ app/src/main/java/com/example/webservices_present/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,15 +24,23 @@ class MainActivity : AppCompatActivity() {
 
         val http = Http(this, findViewById<ProgressBar>(R.id.progressBar))
         val context = this
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        var commentAdapter = CommentAdapter(context, ArrayList())
+        recyclerView.adapter = commentAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             // Launch a coroutine to fetch data asynchronously
             CoroutineScope(Dispatchers.Main).launch { // Use CoroutineScope with Main dispatcher for UI-related coroutines
                 val url = "https://jsonplaceholder.typicode.com/comments/"
-                val jsonResponse = http.fetchData(url)
-//                val jsonResponse = MyRetrofit(findViewById<ProgressBar>(R.id.progressBar)).fetchData(url)
+//                val jsonResponse = http.fetchData(url)
+                val jsonResponse = MyRetrofit(findViewById<ProgressBar>(R.id.progressBar)).fetchData(url)
                 Log.i("MainActivity", "response:$jsonResponse")
+                // Get the type of the response
+                val type = jsonResponse::class.simpleName
+                Log.i("MainActivity", "type:$type")
+                commentAdapter.updateData(jsonResponse as ArrayList<Comment>)
             }
         }
     }
